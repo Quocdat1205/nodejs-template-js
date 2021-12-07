8a905883f98866e0684660d7ee8febb425b9d63a
Quocdat1205 <quocdatsc1205@gmail.com>
2021-12-07 08:43:58 +0700

    add route

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 // require routes
 const { notFound } = require("./handler/responseHandler");
 
+const userTest = [
+  { user1: "nguyen quoc dat", age: 21 },
+  { user2: "hello", age: 22 },
+];
+
 // exports routes
 module.exports = (app) => {
+  app.get("/", (req, res) => {
+    res.send("hello from simple server :)");
+  });
+
+  app.get("/user", (req, res) => {
+    res.json(userTest);
+  });
+
   // all routes not found
   app.use("*", notFound);
 };
